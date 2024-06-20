d5ffb35bf34caae9cc7dec856b7db4eb6aedeb2e
Samer Isaa <samer.isaa1996@gmail.com>
2024-06-20 05:38:21 +0300

    up

--- config/tenancy.php
+++ config/tenancy.php
@@ -60,7 +60,7 @@ return [
          */
         'managers' => [
             'sqlite' => Stancl\Tenancy\TenantDatabaseManagers\SQLiteDatabaseManager::class,
-            'mysql' => Stancl\Tenancy\TenantDatabaseManagers\PermissionControlledMySQLDatabaseManager::class,
+            'mysql' => Stancl\Tenancy\TenantDatabaseManagers\MySQLDatabaseManager::class,
             'pgsql' => Stancl\Tenancy\TenantDatabaseManagers\PostgreSQLDatabaseManager::class,
 
         /**
